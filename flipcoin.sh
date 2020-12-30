bd042b6039f47f5334fb526998842c24bc5d795c
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2020-12-30 14:55:15 +0530

    Update flipcoin.sh

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
-
+##flip the coin and check head or tails
 echo "Well To Flip Coin Simulator"
-
+##taking input from user
 read -p "Do you want to flip a coin (y/n) : " permission
-
+##Computation
 if [ $permission == "y" ]
 then
 	flipResult=$(( RANDOM%2 ))
