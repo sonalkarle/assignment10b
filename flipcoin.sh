8136812f4a24b5dd7ba4001dfdfbb9ad5529cb7a
sonal <sonalkarle01@gmail.com>
2020-12-31 14:17:39 +0530

    Show number of time head and tails win

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,22 +1,25 @@
-<<<<<<< HEAD
-echo "Wlecome to check use case for coin"
-
-=======
 #!/bin/bash -x
+
+noOfHeads=0
+noOfTails=0
 
 echo "Well To Flip Coin Simulator"
 
 read -p "Do you want to flip a coin (y/n) : " permission
 
-if [ $permission == "y" ]
-then
+while [ $permission == "y" ]
+do
 	flipResult=$(( RANDOM%2 ))
 	if [ $flipResult -eq 0 ]
 	then
 		echo "Heads"
+		noOfHeads=$(( $noOfHeads + 1 ))
 	else
 		echo "Tails"
+		noOfTails=$(( $noOfTails + 1 ))
 	fi
-fi
+	read -p "Do you want to flip a coin again (y/n) : " permission
+done
 
->>>>>>> 1_CheckHeadTails_UC1
+echo "Total number of Heads : " $noOfHeads
+echo "Total number of Tails : " $noOfTails
