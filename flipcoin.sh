92afa32a97f971087af5d9b4f367cb8b3e580a08
sonal <sonalkarle01@gmail.com>
2020-12-31 14:30:53 +0530

    Extend UC3 to ensure if its tie than the game continue till the differnce of minimum 2 points is achived

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,30 +1,51 @@
 #!/usr/bin/bash -x
-##Check who will win the match head or tails
-##Variables
+##Extend UC3 to ensure its tie then the game continues till the differnce of minimum 2 point is acheevd
+##variables
 noOfHeads=0
 noOfTails=0
+margin=0
+##Comptation
+function resultCounter ()
+{
+	if [ $1 -eq 0 ]
+		then
+			noOfHeads=$(( $noOfHeads + 1 ))
+		else
+			noOfTails=$(( $noOfTails + 1 ))
+		fi
+}
 
+function checkMargin ()
+{
+	if [ $1 -gt $2 ]
+	then
+		winMargin=$(( $1 - $2 ))
+	else
+		winMargin=$(( $2 - $1 ))
+	fi
+	echo $winMargin
+}
 echo "Welcome To Flip Coin Simulator"
 ##Computation
-while [[ $noOfHeads -ne 21 && $noOfTails -ne 21 ]]
-do
-	flipResult=$(( RANDOM%2 ))
-	if [ $flipResult -eq 0 ]
-	then
-		echo "Heads"
-		noOfHeads=$(( $noOfHeads + 1 ))
-	else
-		echo "Tails"
-		noOfTails=$(( $noOfTails + 1 ))
-	fi
-done
-##Computation
-if [ $noOfHeads -eq 21 ]
+	while [[ $noOfHeads -ne 21 && $noOfTails -ne 21 ]]
+	do
+		flipResult=$(( RANDOM%2 ))
+		resultCounter $flipResult
+	done
+
+margin=$( checkMargin $noOfHeads $noOfTails )
+if [ $margin -eq 0 ]
 then
-	winMargin=$(( $noOfHeads - $noOfTails ))
-	echo "Heads won by $winMargin over Tails"
+	while [ $margin -le 2 ]
+	do
+		flipResult=$(( RANDOM % 2 ))
+		resultCounter $flipResult
+		margin=$( checkMargin $noOfHeads $noOfTails )
+	done
+fi
+if [ $noOfHeads -gt $noOfTails ]
+then
+	echo "Heads Won by Margin of $margin over Tails"
 else
-	
-	winMargin=$(( $noOfTails - $noOfHeads ))
-	echo "Tails won by $winMargin over Heads"
+	echo "Tails Won by Margin of $margin over Heads"
 fi
